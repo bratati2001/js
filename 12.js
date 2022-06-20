d02a48c7157007e7e2f5e9a6db54ab85eee01ac6
Bratati <Bratati Das>
2022-06-20 22:13:21 +0530

    four

---
+++ 12.js
@@ -0,0 +1,23 @@
+var x=10;
+var y=20;
+console.log(x=y);
+console.log(x<=y);
+console.log(x>=y);
+console.log(x>y);
+console.log(x<y);
+var arr =[1,5,"bratati",6,7];
+console.log(arr);
+var a=10;
+var b=5;
+console .log("The value of a+b is " ,a+b);
+console.log("The value of a-b is " ,a-b);
+console.log("The value of a*b is " ,a*b);
+console.log("The value of a/b is " ,a/b);
+var a= "true";
+var b="false";
+console.log(a);
+console.log(b);
+var a="bratati";
+var b="Das";
+console.log(a+b);
+
