8aa292b8e9a76242cf6abeea622e7cd3139c4185
Bratati <Bratati Das>
2022-06-21 15:58:21 +0530

    four

---
+++ 14.js
@@ -0,0 +1,18 @@
+let mystring="my name is Bratati Das";
+console.log(mystring);
+console.log(mystring.length);
+console.log(mystring.indexOf("Das"));
+
+
+let mydate=new Date();
+console.log(mydate);
+console.log(mydate.getFullYear());
+console.log(mydate.getDay());
+console.log(mydate.getHours());
+console.log(mydate.getMilliseconds());
+console.log(mydate.getMonth());
+console.log(mydate.getMinutes());
+console.log(mydate.getTime());
+console.log(mydate.getSeconds());
+console.log(mydate.getTimezoneOffset());
+console.log(mydate.getUTCDate());
